faaa81036c6860fe654bba99ce57e128057e4102
adam <adam@triazo.net>
2014-07-26 10:49:44 -0400

    Adds slashes to the names of directories

--- gms.go
+++ gms.go
@@ -1,113 +1,122 @@
 package main
 
 import (
-        "io/ioutil"
-        "fmt"
-        "net/http"
-        "os"
-        "path/filepath"
+    "io/ioutil"
+    "fmt"
+    "net/http"
+    "os"
+    "path/filepath"
 )
 
 var visits int = 0
 
 func linkify(filename string) (string) {
-        var r string = ""
-        r += "<a href=\"" + filename + "\">" + filename + "</a>"
-        return r
+    var r string = ""
+    r += "<a href=\"" + filename + "\">" + filename + "</a>"
+    return r
 }
 
 func tagify(text string, tag string, attributes string) (string) {
-        var r string = "<" + tag + " " + attributes + ">" + text + "</" + tag + ">"
-        return r
+    var r string = "<" + tag + " " + attributes + ">" + text + "</" + tag + ">"
+    return r
 }
 
 func getHostname() (string) {
-        hostname, _ := os.Hostname()
-        return hostname
+    hostname, _ := os.Hostname()
+    return hostname
 }
 
-func isDirictery(path string) (bool) {
-        f, err := os.Open(path)
-        if err != nil {
-                fmt.Printf("Cannot open file %s\n", request)
-                // Dunno? Return 404?
-        }
+func isDirectory(path string) (bool, error) {
+    // Returns true if the given path is a directory
+    f, err := os.Open(path)
+    if err != nil {
+        fmt.Printf("Cannot open file %s\n", path)
+        return false, err
+        // Dunno? Return 404?
+    }
 
-        fi, err := f.Stat()
-        if err != nil {
-                fmt.Printf("Error Stating file %s\n", request)
-                // How could this error???
-        }
+    fi, err := f.Stat()
+    defer f.Close()
+    if err != nil {
+        return false, err
+        fmt.Printf("Error Stating file %s\n", path)
+        // How could this error???
+    }
 
-        switch mode := fi.mode(); {
-        case mode.IsDir():
-                return true
-        case mode.IsRegular():
-                return false
-        }
-	f.close()
+    var dir bool = false
+    if fi.Mode().IsDir() {
+        return true, nil
+    }
+    
+    return dir, nil
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
-        // Get a full path to the destanation file
-        wd, _ := os.Getwd()
-        working_dir, _ := filepath.Abs(wd)
-        var request string = working_dir + r.URL.Path
-        fmt.Printf("%s\n",request)
+    // Get a full path to the destanation file
+    wd, _ := os.Getwd()
+    working_dir, _ := filepath.Abs(wd)
+    var request string = working_dir + r.URL.Path
+    fmt.Printf("%s\n",request)
 
-        // Detect file type
-        f, err := os.Open(request)
-        if err != nil {
-                fmt.Printf("Cannot open file %s\n", request)
-                // Dunno? Return 404?
-        }
-        defer f.Close()
-        fi, err := f.Stat()
-        if err != nil {
-                fmt.Printf("Error Stating file %s\n", request)
-                // How could this error???
-        }
-        switch mode := fi.Mode(); {
-        case mode.IsDir():
-                // ====================================================================
-                // ==== Stuff for a Directory
+    mode, err := isDirectory(request)
+    if err != nil {
+        // 404 Should occur here somehow
+        return
+    }
 
-                // Get a directory listing
-                files, dir_read_error := ioutil.ReadDir(working_dir + r.URL.Path)
-                if dir_read_error != nil {
-                        fmt.Printf("Error reading %s: %d\n", r.URL.Path, dir_read_error)
-                }
+    if mode {
+        // ====================================================================
+        // ==== Stuff for a Directory
 
-                fmt.Fprintf(w,"<html>\n")
-                fmt.Fprintf(w,"<head>\n")
-                fmt.Fprintf(w,"  <script type='text/javascript' src='script.js'></script>\n")
-                fmt.Fprintf(w,"</head>\n")
-                fmt.Fprintf(w,"<body>\n")
-                fmt.Fprintf(w,"  <span id='count'>%d</span> Visits so far\n", visits)
-                fmt.Fprintf(w,"  <ul>\n")
-                for _, f := range files {
-                        fmt.Fprintf(w,"    %s\n",tagify(linkify(f.Name()),"li", ""))
-                }
-                fmt.Fprintf(w,"  </ul>\n")
-                fmt.Fprintf(w,"</body>\n</html>")
-        case mode.IsRegular():
-                // TODO: read byte by byte
-                fb, _ := ioutil.ReadFile(request)
-                fmt.Fprintf(w, string(fb))
+        // Check if there is a trailing slash.  If not, redirect
+
+        // At this point you can assume there is a slash
+
+        // Get a directory listingp
+        files, dir_read_error := ioutil.ReadDir(request)
+        if dir_read_error != nil {
+            fmt.Printf("Error reading %s: %d\n", r.URL.Path, dir_read_error)
         }
 
-        //fmt.Printf("Handling connection number %d\n", visits)
-        visits += 1
+        fmt.Fprintf(w,"<html>\n")
+        fmt.Fprintf(w,"<head>\n")
+        fmt.Fprintf(w,"  <script type='text/javascript' src='script.js'></script>\n")
+        fmt.Fprintf(w,"</head>\n")
+        fmt.Fprintf(w,"<body>\n")
+        fmt.Fprintf(w,"  <span id='count'>%d</span> Visits so far\n", visits)
+        fmt.Fprintf(w,"  <ul>\n")
+        for _, f := range files {
+            dir, err:=isDirectory(request+f.Name())
+            if err != nil {
+                fmt.Printf("Error reading file: %s", request+f.Name())
+                // How can this happen?
+            }
+            if dir {
+                fmt.Fprintf(w,"    %s\n",tagify(linkify(f.Name()+"/"),"li", ""))
+            } else {
+                fmt.Fprintf(w,"    %s\n",tagify(linkify(f.Name()),"li", ""))
+            }
+        }
+        fmt.Fprintf(w,"  </ul>\n")
+        fmt.Fprintf(w,"</body>\n</html>")
+    } else {
+        // TODO: read byte by byte
+        fb, _ := ioutil.ReadFile(request)
+        fmt.Fprintf(w, string(fb))
+    }
+
+    //fmt.Printf("Handling connection number %d\n", visits)
+    visits += 1
 }
 
 func scriptGetter(w http.ResponseWriter, r *http.Request) {
-        fi, _ := ioutil.ReadFile("script.js")
-        fmt.Fprintf(w, string(fi))
+    fi, _ := ioutil.ReadFile("script.js")
+    fmt.Fprintf(w, string(fi))
 }
 
 func main() {
-        http.HandleFunc("/", handler)
-        http.HandleFunc("/script.js", scriptGetter)
-        http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/", handler)
+    http.HandleFunc("/script.js", scriptGetter)
+    http.ListenAndServe(":8080", nil)
 }
